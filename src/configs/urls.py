e9e78bb5e1a72e69a6ec0f799b6dfb38478a3f42
Timur Zautadze <timurza@icloud.com>
2023-04-24 12:53:59 +0300

    added default_version

--- src/configs/urls.py
+++ src/configs/urls.py
@@ -21,11 +21,12 @@ from drf_yasg import openapi
 from rest_framework import permissions
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="API",
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+    openapi.Info(
+        title="API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [

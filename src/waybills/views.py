97c3bdb862238170c53fe2c28a7ac18c2bc80002
timur <timurza@icloud.com>
2023-05-23 21:22:30 +0300

    added waybills filter

--- src/waybills/views.py
+++ src/waybills/views.py
@@ -1,4 +1,5 @@
 from common.views import APIViewSet
+from waybills.filters import WaybillFilter
 from waybills.models import Waybill, Image, File
 from waybills.serializers import (
     WaybillSerializer, ImageSerializer, FileSerializer,
@@ -8,6 +9,7 @@ from waybills.serializers import (
 class WaybillAPI(APIViewSet):
     queryset = Waybill.objects.all()
     serializer_class = WaybillSerializer
+    filterset_class = WaybillFilter
     my_tags = ['waybills']
 
 
